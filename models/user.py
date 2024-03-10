173b93b622054cd0fcd7f9844449cda713cf7827
Peacechimamaka <Peaceamaka111@gmail.com>
2024-03-10 20:36:40 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,9 +1,18 @@
-#!/usr/bin/python
-'''Module that stores information about the user class'''
+#!/usr/bin/python3
+'''Defines the class user'''
+from base_model import BaseModel
 
 class User(BaseModel):
-    '''A class user that inherits from BaseModel and 
-    has some public class attributes passed to it '''
+    '''
+    A class User.
+
+    Attributes:
+
+        email (str): The user's email.
+        password : The user's password
+        first_name: The first name of the user
+        last_name: The last name of the user
+        '''
 
     email = " "
     password = " "
