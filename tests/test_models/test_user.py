bf5c88061bb3d357fc1daa65b9aa29406057536e
Peacechimamaka <Peaceamaka111@gmail.com>
2024-03-12 19:39:44 +0100

    test_user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''This module tests for the user class'''
+
+from models.user import User
+import unittest
+
+class TestUser(unittest.TestCase):
+    '''TestUser tests the attributes 
+    of the user class'''
+
+    def setUp(self):
+        '''this setup method initializes
+        a User instance to each test case'''
+        self.instance = User()
+
+    def test_email(self):
+        '''test_email tests the email of the user'''
+        self.assertEqual(type(self.instance.email), str)
+        self.assetEqual(self.instance.email, '')
+
+    def test_password(self):
+        '''test_password tests the password of the user'''
+        self.assertIsInstance(self.instance.password, str)
+        self.assertEqual(self.instance.password, '')
+
+    def test_first_name(self):
+        '''test_first_name tests the first name of the user'''
+        self.assertIsInstance(self.instance.first_name, str)
+        self.assertEqual(self.instance.first_name, '')
+
+    def test_last_name(self):
+        '''test_last_name tests the last_name of the user'''
+        self.assertIsInstance(self.instance.last_name, str)
+        self.assertEqual(self.instance.last_name, '')
+
+if __name__ == '__main__':
+    unittest.main()
