7291aa6a06f1630d52202e72551e439d0b2d24a5
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-03-12 10:22:18 +0100

    Fixed issues in user.py

--- models/user.py
+++ models/user.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-'''Defines the class user'''
-from base_model import BaseModel
+"""
+Defines the class user
+"""
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''
+    """
     A class User.
 
     Attributes:
-
         email (str): The user's email.
-        password : The user's password
-        first_name: The first name of the user
-        last_name: The last name of the user
-        '''
+        password (str): The user's password
+        first_name (str): The first name of the user
+        last_name (str): The last name of the user
+    """
 
-    email = " "
-    password = " "
-    first_name = " "
-    last_name = " "
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
